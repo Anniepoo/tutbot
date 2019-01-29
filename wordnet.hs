e0cb48ae6bfd5f673f8ea5a4d294f1442d5852ec
Anne Ogborn <annie66us@yahoo.com>
2019-01-29 09:49:56 +0530

    nonworking attempt to make hash tables

--- wordnet.hs
+++ wordnet.hs
@@ -1,6 +1,32 @@
 module WordNet ( load_word_net )  where
 import System.IO
+import qualified Data.HashTable.IO as H
 
+-- Using concrete type makes it easier for ghc
+-- to optimize
+type HashTable k v = H.BasicHashTable k v
+
+-- s(synset_id,w_num,’word’,ss_type,sense_number,tag_count).
+data SRecord = SRecord {
+               wordNum :: Int,
+               word :: String,
+               synsetType :: Char,
+               sense :: Int,
+               tagged :: Int -- # times sense in concordance
+               }
+
+data WordNetSTable = HashTable Int SRecord
+
+wordnetS :: WordNetSTable
+wordnetS = WordNetSTable
+
+-- hyp(synset_id,synset_id).
+-- The hyp operator specifies that the second synset is a
+-- hypernym of the first synset.
+data WordNetHypernymTable = HashTable Int Int
+
+wordnetH :: WordNetHypernymTable
+wordnetH  = WordNetHypernymTable
 
 load_word_net :: FilePath -> IO ()
 load_word_net filename =
